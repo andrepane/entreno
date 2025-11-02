c166b1e9c945923835e8355b4a243a9849d3c971
andrepane <156331723+andrepane@users.noreply.github.com>
2025-11-02 10:50:09 +0100

    Add PWA support for Entrenos app

--- app.js
+++ app.js
@@ -565,3 +565,12 @@ mcNext.addEventListener("click", ()=>{
   mcRefDate.setMonth(mcRefDate.getMonth()+1);
   renderMiniCalendar();
 });
+
+/* ========= PWA ========= */
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .catch((err) => console.warn('Error registrando el service worker', err));
+  });
+}
