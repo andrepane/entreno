898e1b1981253b766b5388fe793605e62eb02129
andrepane <156331723+andrepane@users.noreply.github.com>
2026-01-13 12:04:35 +0100

    Update and rename firebase-config.example.js to firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Copia este archivo como firebase-config.js y rellena tus credenciales de Firebase.
+// Este archivo debe exponer window.FIREBASE_CONFIG para que app.js pueda inicializar la sincronización.
+  const firebaseConfig = {
+    apiKey: "AIzaSyD0H3VjoBBEJonJEWj8l4lm79dKuMaoyw4",
+    authDomain: "entreno-57806.firebaseapp.com",
+    projectId: "entreno-57806",
+    storageBucket: "entreno-57806.firebasestorage.app",
+    messagingSenderId: "718935927574",
+    appId: "1:718935927574:web:503a9783911a837781be37"
+  };
